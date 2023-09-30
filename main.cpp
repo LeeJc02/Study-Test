cac583afb58d62ce93dca54b1afde75eaf5fb5f8
LeeJc02 <129182487+LeeJc02@users.noreply.github.com>
2023-09-30 05:32:33 +0000

    new commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
+int Fact(int n){
+  if(n==0)
+    return 1;
+  else
+    return n*Fact(n-1);
+}
 int main(){
-  std::cout<<"hgdcjgc";
+  std::cout<<Fact(10)<<std::endl;
   return 0;
 }
